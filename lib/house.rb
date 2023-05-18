01b9fcaf31bae4a1d3d658d55382727ee8fba562
Connor Richmond <richmond.connor@gmail.com>
2023-05-18 12:05:10 -0400

    Add method to add rooms to house

--- lib/house.rb
+++ lib/house.rb
@@ -2,9 +2,13 @@ class House
 
   attr_reader :price, :address, :rooms
 
-  def initialize(price, address, rooms)
-    @price = price
+  def initialize(price, address, rooms = [])
+    @price = price.delete('$').to_i
     @address = address
     @rooms = rooms
+  end
+
+  def add_room(data)
+    rooms << data
   end
 end
