66797f7097552dae3a3676494c2fe7f2bdfa646b
Connor Richmond <richmond.connor@gmail.com>
2023-05-18 11:38:20 -0400

    Complete class 'Room' for iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,20 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width, :is_painted
 
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
   end
+
+  def area
+    length.to_i * width.to_i
+  end
+
+  def paint
+    @is_painted = true
+  end
+
 end
