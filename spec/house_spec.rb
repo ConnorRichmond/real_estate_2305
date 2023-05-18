11133cd486c38c63c7a67fa747d94b0c44706c90
Connor Richmond <richmond.connor@gmail.com>
2023-05-18 12:05:24 -0400

    Test for house class

---
+++ spec/house_spec.rb
@@ -0,0 +1,23 @@
+require './lib/room'
+require './lib/house'
+
+describe House do
+
+  it 'exists' do
+    house = House.new('$400000', "123 sugar lane")
+
+    expect(house.price).to eq(400000)
+    expect(house.address).to eq('123 sugar lane')
+    expect(house.rooms).to eq([])
+  end
+
+  it 'has rooms' do
+    house = House.new('$400000', "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    expect(house.rooms).to contain_exactly(room_1, room_2)
+  end
+end
